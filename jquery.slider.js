492a289f98f4147b7f23c9ceb5234b01cd19b5ab
何文东 <teen@163.com>
2015-02-10 17:06:19 +0800

    Update jquery.slider.js

--- jquery.slider.js
+++ jquery.slider.js
@@ -9,10 +9,10 @@
 			direction: "x", //滚动放心
 			next: ".J_next", //下一个按钮
 			prev: ".J_prev", //上一个按钮
-			disableClass: "disabled", //禁止上下滚动点击
+			clickloop: true, //是否禁止上下点击循环滚动
 			auto: 5000, //是否自动滚动,动画间隔延迟时间 如果时间为0 关闭自动滚动
 			speed: 500, //动画时间
-			// loop:false,  //是否循环
+			//loop:false,  //是否循环
 			scrollnum: 4, //同时滚动个数
 			itemview: 4, //显示个数
 			itemWarp: '.itemWarp', //显示item列表的warp
@@ -30,8 +30,10 @@
 				width = opt.itemwidth * len, 	//图总宽度
 				height = opt.itemheight * len, 	//图总高度
 				scrollX = opt.itemwidth * opt.scrollnum,  //一次滚动宽度
-				scrollY = opt.itemheight * opt.scrollnum;  //一次滚动高度
-
+				scrollY = opt.itemheight * opt.scrollnum,  //一次滚动高度
+				maxTop = height - W_height,			//最大垂直偏移
+				maxLeft = width - W_width;			//最大水平偏移
+						
 				//初始化宽度和高度
 				if(opt.direction == "x"){//水平滚动
 					$(this).width(W_width).height(opt.itemheight)
@@ -51,8 +53,7 @@
 				if(_this.find(opt.itemWarp).is(":animated")) return false;
 
 				if(opt.direction == 'y'){
-					var maxTop = height - W_height,
-					    marginTop = parseInt(_this.find(opt.itemWarp).css('margin-top')),
+					var marginTop = parseInt(_this.find(opt.itemWarp).css('margin-top')),
 					    result = 0;
 					if(maxTop > 0 ){
 						if(type == 'prev'){
@@ -73,8 +74,7 @@
 					_this.find(opt.itemWarp).animate({"margin-top":result+"px"}, opt.speed);
 
 				}else{//dirction == x default
-					var maxLeft = width - W_width,
-						marginLeft = parseInt(_this.find(opt.itemWarp).css('margin-left')),
+					var marginLeft = parseInt(_this.find(opt.itemWarp).css('margin-left')),
 						result = 0;
 					if(maxLeft > 0 ){
 						if(type == 'prev'){
@@ -99,10 +99,20 @@
 			
 			// next
 			$(opt.next).on('click', function(){
+				if(!opt.clickloop){ //不循环点击滚动
+				var marginTop = parseInt(_this.find(opt.itemWarp).css('margin-top')),
+					marginLeft = parseInt(_this.find(opt.itemWarp).css('margin-left'));
+				if( (opt.direction =='y' && marginTop == -maxTop) || (opt.direction == 'x' && marginLeft == -maxLeft) ) return false;
+				}
 				_scroll('next');
 			});
 			//pre
 			$(opt.prev).on('click', function(){
+				if(!opt.clickloop){//不循环点击滚动
+				var marginTop = parseInt(_this.find(opt.itemWarp).css('margin-top')),
+					marginLeft = parseInt(_this.find(opt.itemWarp).css('margin-left'));
+				if( (opt.direction =='y' && marginTop == 0 ) || (opt.direction == 'x' && marginLeft == 0) ) return false;
+				}
 				_scroll('next');
 			});
 			var timer;
